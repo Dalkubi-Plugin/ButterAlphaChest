82abe18ca19e83266269bdf996cf97658e1e44c5
달쿠비 <dydvy08@gmail.com>
2024-10-22 23:09:39 +0900

    Initial commit

---
+++ src/main/java/kr/dalkubi/butterAlphaChest/ButterAlphaChest.java
@@ -0,0 +1,16 @@
+package kr.dalkubi.butterAlphaChest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ButterAlphaChest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
